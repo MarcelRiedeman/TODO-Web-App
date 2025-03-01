e0dc52abbf44fcb4446da4fafbd79d906b202a7b
Marcel <marcel.riedeman@gmail.com>
2025-03-01 14:57:16 +0100

    add Task model

---
+++ Models/Task.cs
@@ -0,0 +1,16 @@
+﻿namespace TodoWebApp.Models
+{
+    public class Task
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Task()
+        {
+            
+        }
+    }
+}
